78d737505f6aa2f7cf757442d0eb1dfae4223aac
Anushka Beri <berianushka@gmail.com>
2020-01-28 16:47:02 +0530

    Gallery page redoing

--- src/GalleryPage.js
+++ src/GalleryPage.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 import FooterOther from "./components/FooterOther";
 import HeadingUnderline from "./components/HeadingUnderline";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class GalleryPage extends Component {
+//   $(window).load(function(){
+//     var $container = $('.portfolioContainer');
+//     $container.isotope({
+//         filter: '*',
+//         animationOptions: {
+//             duration: 750,
+//             easing: 'linear',
+//             queue: false
+//         }
+//     });
+ 
+//     $('.portfolioFilter a').click(function(){
+//         $('.portfolioFilter .current').removeClass('current');
+//         $(this).addClass('current');
+ 
+//         var selector = $(this).attr('data-filter');
+//         $container.isotope({
+//             filter: selector,
+//             animationOptions: {
+//                 duration: 750,
+//                 easing: 'linear',
+//                 queue: false
+//             }
+//          });
+//          return false;
+//     }); 
+// });
   render() {
     return (
       <>
@@ -11,15 +38,33 @@ export default class GalleryPage extends Component {
           <h1 className="is-size-1 has-text-centered">Gallery</h1>
           <HeadingUnderline />
           <div className="container">
+            <div class="portfolioFilter clearfix">
+              <a href="#" data-filter="*" class="current">
+                All Categories
+              </a>
+              <a href="#" data-filter=".webTemplates">
+                Spa
+              </a>
+              <a href="#" data-filter=".logos">
+                Makeup
+              </a>
+              <a href="#" data-filter=".drawings">
+                Hairdo
+              </a>
+              <a href="#" data-filter=".ui">
+                Cosmetics
+              </a>
+            </div>
+
             <div className="gallery-images">
               {imagesList.map(images => (
-                <>
+                <div className={images.category}>
                   <img
                     src={images.imageLink}
                     alt="gallery-placeholder"
                     className="gallery-picture"
                   />
-                </>
+                </div>
               ))}
             </div>
             <div className="container button-contain">
@@ -42,39 +87,51 @@ export default class GalleryPage extends Component {
 
 const imagesList = [
   {
+    category: "makeup",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "hair",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "saloon",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "cosmetics",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "makeup",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "hair",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "saloon",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "cosmetics",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "makeup",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "hair",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "saloon",
     imageLink: "https://via.placeholder.com/300"
   },
   {
+    category: "cosmetics",
     imageLink: "https://via.placeholder.com/300"
   }
 ];
