d6b0d29acab4caf24d6d4dcaf2326d06580f13fc
Anushka Beri <berianushka@gmail.com>
2020-01-28 15:17:28 +0530

    Services page altered

--- src/components/HeroPanel.js
+++ src/components/HeroPanel.js
@@ -8,10 +8,6 @@ export default class HeroPanel extends Component {
           <div className="hero-body">
             <div className="container">
               <h1 className="is-size-1 has-text-white-bis" >Aadya Beauty Saloon and Spa</h1>
-              <h2 className="subtitle has-text-black-ter">
-                Rest & Relax. Enjoy some much needed me time during a three-hour
-                spa session.
-              </h2>
             </div>
           </div>
         </div>
