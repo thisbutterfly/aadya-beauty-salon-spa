96a1ed9f4d340ce8411b51181d1a4e5b247c3255
Anushka Beri <berianushka@gmail.com>
2020-01-31 13:57:13 +0530

    form working

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import DatePicker from "react-date-picker";
+import axios from "axios";
+
+const API_PATH = "http://localhost:3000/react-contact-form/api/index.php";
 export default class BookingForm extends Component {
   state = {
     date: new Date(),
-    selected: ""
+    selected: "",
+    name: "",
+    email: "",
+    phoneNumber: "",
+    service: "",
+    mailSent: false,
+    error: null
   };
   onChange = (selected, date) => {
     this.setState({ selected, date });
   };
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    axios({
+      method: "post",
+      url: `${API_PATH}`,
+      headers: { "content-type": "application/json" },
+      data: this.state
+    })
+      .then(result => {
+        this.setState({
+          mailSent: result.data.sent
+        });
+      })
+      .catch(error => this.setState({ error: error.message }));
+  }
   render() {
     return (
-      <form>
+      <form action="/action_page.php">
         <div className="field">
           <label className="label">Name</label>
           <div className="control">
@@ -19,6 +44,8 @@ export default class BookingForm extends Component {
               type="text"
               placeholder="Enter your name"
               required
+              value={this.state.name}
+              onChange={e => this.setState({ name: e.target.value })}
             />
           </div>
         </div>
@@ -30,6 +57,8 @@ export default class BookingForm extends Component {
               type="text"
               placeholder="Enter your email"
               required
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
             />
           </div>
         </div>
@@ -41,19 +70,25 @@ export default class BookingForm extends Component {
               type="text"
               placeholder="Enter your phone number"
               required
+              value={this.state.phoneNumber}
+              onChange={e => this.setState({ phoneNumber: e.target.value })}
             />
           </div>
         </div>
         <br />
         <label className="label">Select Services</label>
 
-        {servicesList.map(services=>(
-        <div className="">
-          <label class="checkbox">
-            <input type="checkbox" />
-            {services.itemName}
-          </label>
-        </div>
+        {servicesList.map(services => (
+          <div className="">
+            <label class="checkbox">
+              <input
+                type="checkbox"
+                value={this.state.service}
+                onChange={e => this.setState({ service: e.target.value })}
+              />
+              {services.itemName}
+            </label>
+          </div>
         ))}
 
         <div className="field">
@@ -61,53 +96,61 @@ export default class BookingForm extends Component {
           <label className="label">Choose Date</label>
           <DatePicker onChange={this.onChange} value={this.state.date} />
         </div>
+        <button
+          className="appointment-button"
+          onClick={e => this.handleFormSubmit(e)}
+        >
+          Submit
+        </button>
+        <div>
+          {this.state.mailSent && <div>Thank you for contcting us.</div>}
+        </div>
       </form>
     );
   }
 }
 
-
 const servicesList = [
   {
-    itemName : "Face Bleach"
+    itemName: "Face Bleach"
   },
   {
-    itemName : "De Tan"
+    itemName: "De Tan"
   },
   {
-    itemName : "Half arms wax"
+    itemName: "Half arms wax"
   },
   {
-    itemName : "Full arms wax"
+    itemName: "Full arms wax"
   },
   {
-    itemName : "Half legs wax"
+    itemName: "Half legs wax"
   },
   {
-    itemName : "Full legs wax"
+    itemName: "Full legs wax"
   },
   {
-    itemName : "Threading upplerlips"
+    itemName: "Threading upplerlips"
   },
   {
-    itemName : "Threading eyebrows"
+    itemName: "Threading eyebrows"
   },
   {
-    itemName : "Upperlips waxing"
+    itemName: "Upperlips waxing"
   },
   {
-    itemName : "Manicure"
+    itemName: "Manicure"
   },
   {
-    itemName : "Pedicure"
+    itemName: "Pedicure"
   },
   {
-    itemName : "Face Bleach"
+    itemName: "Face Bleach"
   },
   {
-    itemName : "Oil Hair massage"
+    itemName: "Oil Hair massage"
   },
   {
-    itemName : "Hair Spa"
+    itemName: "Hair Spa"
   }
-]
+];
