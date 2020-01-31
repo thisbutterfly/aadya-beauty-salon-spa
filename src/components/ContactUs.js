e38ef0d826562ec3473021f186fffc1015296fad
Anushka Beri <berianushka@gmail.com>
2020-01-31 11:25:49 +0530

    contact links added

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -10,11 +10,19 @@ export default class ContactUs extends Component {
         <div className="contact-container columns">
           <div className="contact-wrap column has-text-weight-bold">
             <p className="header is-size-4">Call us at: </p>
-            <p className="subtitle is-size-5">91-9305164178</p>
+            <a href="tel:9305164178">
+              <p className="subtitle is-size-5">91-9305164178</p>
+            </a>
           </div>
           <div className="contact-wrap column has-text-weight-bold">
             <p className="header is-size-4">Email us: </p>
-            <p className="subtitle is-size-5">aadyabeautyadda@gmail.com</p>
+            <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=aadyabeautyadda@gmail.com"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <p className="subtitle is-size-5">aadyabeautyadda@gmail.com</p>
+            </a>
           </div>
           <div className="contact-wrap-two column has-text-weight-bold">
             <p className="header is-size-4">Address: </p>
